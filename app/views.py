36cd41754cc36f19234cb68c2cb4252473ebea86
Tyler Huntington <thuntin1@swarthmore.edu>
2017-08-27 21:40:13 -0400

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+
+	return render_template('fbd_webmap.html')
+
+
---
+++ flask/bin/flask_packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pip install flask-whooshalchemy
+pip install flask-wtf
+pip install flask-babel
+pip install guess_language
+pip install flipflop
+pip install coverage			
